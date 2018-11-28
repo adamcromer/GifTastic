d48c807e44244a28eaa0d27bf2f92b3296d3fbf3
adamjorycromer <adamjorycromer@gmail.com>
2018-11-27 21:15:11 -0700

    working on javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
 
-//An array with a list of cartoons to show gifs for.
-var cartoonList = ["Adventure Time", "The Simpsons", "Rick and Morty", "Regular Show", "Bob's Burgers", "Futurama", "Steven Universe", "Archer", "Avatar: The Last Airbender", "Pokemon", "Rugrats", "SpongeBob SquarePants", "Popeye", "Dexter's Laboratory", ];
-cartoonList.sort();
+    //An array with a list of cartoons to show gifs for.
+    var cartoonList = ["Adventure Time", "The Simpsons", "Rick and Morty", "Regular Show", "Bob's Burgers", "Futurama", "Steven Universe", "Archer", "Avatar: The Last Airbender", "Pokemon", "Rugrats", "SpongeBob SquarePants", "Popeye", "Dexter's Laboratory", "Tom and Jerry", "Powerpuff Girls"];
+    cartoonList.sort();
 
-//Global var that link to their $counterparts.
-var btnHolder = $("#btnHolder");
 
-for (i = 0; i < cartoonList.length; i++) {
-    var cartoon = $('<button type="button" class="btn btn-success p-2">').text(cartoonList[i]);
-    btnHolder.append(cartoon);
-}
+    //Global var that link to their $counterparts.
+    var btnHolder = $("#btnHolder");
+    var button = $("button");
+    var mainText = $("#mainText");
+    
 
+    //A for loop which takes each item from the array and makes it into a button.
+    for (i = 0; i < cartoonList.length; i++) {
+        var cartoon = $('<button type="button" class="btn btn-success p-2">').text
+        (cartoonList[i]);
+        // cartoon.id('button' + i);
+
+        btnHolder.append(cartoon);
+    }
+
+
+    //A function for what happens when you click on a button.
+    button.click(function () {
+        console.log("testq");
+        console.log(this.text());
+        
+
+    });
 });
