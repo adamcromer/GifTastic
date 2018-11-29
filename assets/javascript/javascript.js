5abfe1debfbaf96ba60575e746abc32cee94af76
adamjorycromer <adamjorycromer@gmail.com>
2018-11-28 17:46:48 -0700

    found direct link through response

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
     //A for loop which takes each item from the array and makes it into a button.
     for (i = 0; i < cartoonList.length; i++) {
-        var cartoon = $('<button type="button" class="btn btn-success p-2">').text
+        var cartoon = $('<button type="button" class="btn btn-success p-2 m-1">').text
             (cartoonList[i]);
         cartoon.attr('id', cartoonList[i]);
         btnHolder.append(cartoon);
@@ -25,11 +25,18 @@ $(document).ready(function () {
 
     function clickFunction() {
         button = $(".btn");
-        button.on("click", function () {
+        button.click(function () {
             console.log(this.id);
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + this.id +"&api_key=3cIsIeloVUjq1nketG9GZn3hRsKSFI3S&limit=10";
+            console.log(queryURL);
 
-            
-
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                console.log(response.data[0].embed_url);
+            });
         });
     }
 });
