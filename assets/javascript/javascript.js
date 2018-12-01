0b434b2e7399f7f3946ea54fee0f47e31ae0bc26
adamjorycromer <adamjorycromer@gmail.com>
2018-11-30 17:18:06 -0700

    testing hover

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     //An array with a list of cartoons to show gifs for.
-    var cartoonList = ["Adventure Time", "The Simpsons", "Rick and Morty", "Regular Show", "Bob's Burgers", "Futurama", "Steven Universe", "Archer", "Avatar: The Last Airbender", "Pokemon", "Rugrats", "SpongeBob SquarePants", "Popeye", "Dexter's Laboratory", "Tom and Jerry", "Powerpuff Girls", "King of The Hill", "Garfield"];
+    var cartoonList = ["Adventure Time", "The Simpsons", "Rick and Morty", "Regular Show", "Bob's Burgers", "Futurama", "Steven Universe", "Archer", "Avatar: The Last Airbender", "Pokemon", "Rugrats", "SpongeBob SquarePants", "Popeye", "Dexter's Laboratory", "Tom and Jerry", "Powerpuff Girls", "King of The Hill", "Garfield", "Johnny Bravo", "Batman", "Spider-Man: The Animated Series", "X-Men: The Animated Series", "Scooby-Doo", "Jonny Quest", "Transformers", "Teenage Mutant Ninja Turtles"];
     cartoonList.sort();
 
 
@@ -9,15 +9,16 @@ $(document).ready(function () {
     var btnHolder = $("#btnHolder");
     var button = $(".btn");
     var mainText = $("#mainText");
+    var gif = $(".gif");
 
 
     //A for loop which takes each item from the array and makes it into a button.
     for (var i = 0; i < cartoonList.length; i++) {
-        var cartoon = $('<button type="button" class="btn btn-success p-2 mr-1 ml-1">').text(cartoonList[i]);
+        var cartoon = $('<button type="button" class="btn btn-success p-2 mr-1 ml-1 float-left">').text(cartoonList[i]);
         cartoon.attr('data-name', cartoonList[i]);
         btnHolder.append(cartoon);
         button = $(".btn");
-        var gif = $(".gif");
+
     }
 
 
@@ -36,20 +37,31 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-            for (var i = 1; i <= 10; i++) {
+            for (var i = 0; i < 10; i++) {
                 console.log(response);
-                var gifDiv = $("<img>")
-                gifDiv.attr("src", response.data[i].images.fixed_width_still.url);
-                var newDiv = $("<div>").text((response.data[i].title).toUpperCase());
-                mainText.append(newDiv);
+                var gifDiv = $("<img class='special m-2'>");
+                gifDiv.attr("data-name", i);
+                gifDiv.attr("src", response.data[i].images.fixed_height_still.url);
                 mainText.append(gifDiv);
+                gif = $(".special");
             }
         });
     });
+    
+    gif.mouseenter(function () {
 
-    var gifDiv = $("<img>")
-    gifDiv.attr("src", "https://giphy.com/embed/39Zs6k1mvNkZ2");
-    mainText.append(gifDiv);
+        console.log("test");
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function (response) {
+            
+        //     var thisIndex = $(this).attr("data-name");
+        //     console.log(thisIndex);
+        //     gif.attr("src", response.data[thisIndex].images.fixed_height.url);
+        // });
+    });
+
 
 
 });
